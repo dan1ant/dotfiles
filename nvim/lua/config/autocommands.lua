5b580473d41a14d66feca8e8ce16f80d2705275a
Daniele Antimi <daniele.antimi@gmail.com>
2024-03-26 13:10:14 +0100

    Add(nvim): Telescope Keymaps

--- nvim/lua/config/autocommands.lua
+++ nvim/lua/config/autocommands.lua
@@ -2,30 +2,64 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
   pattern = '*',
-  callback = function() vim.highlight.on_yank() end,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(event)
-    local opts = { remap = false, buffer = event.buf }
+    local bufnr = event.buf
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
 
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<A-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
     vim.keymap.set(
       'n',
-      '<leader>wl',
-      function() vim.notify(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
-      opts
+      'gd',
+      require('telescope.builtin').lsp_definitions,
+      { desc = '(LSP) Definitions', buffer = event.buf }
     )
-    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set(
+      'n',
+      'gr',
+      require('telescope.builtin').lsp_references,
+      { desc = '(LSP) References', buffer = event.buf }
+    )
+    vim.keymap.set(
+      'n',
+      'gI',
+      require('telescope.builtin').lsp_implementations,
+      { desc = '(LSP) Implementations', buffer = event.buf }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>D',
+      require('telescope.builtin').lsp_type_definitions,
+      { desc = '(LSP) Type Definitions', buffer = event.buf }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>ds',
+      require('telescope.builtin').lsp_document_symbols,
+      { desc = '(LSP) Document Symbols', buffer = event.buf }
+    )
+    vim.keymap.set(
+      'n',
+      '<leader>ws',
+      require('telescope.builtin').lsp_dynamic_workspace_symbols,
+      { desc = '(LSP) Workspace Symbols', buffer = event.buf }
+    )
+
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = '(LSP) Declaration', buffer = event.buf })
+    vim.keymap.set('n', '<A-k>', vim.lsp.buf.signature_help, { desc = '(LSP) Signature Help', buffer = event.buf })
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = '(LSP) Hover', buffer = event.buf })
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = '(LSP) Rename', buffer = event.buf })
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = '(LSP) Code Actions', buffer = event.buf })
+
+    if client ~= nil and client.server_capabilities.inlayHintProvider then
+      vim.keymap.set('n', '<leader>ih', function()
+        vim.lsp.inlay_hint.enable(bufnr, not vim.lsp.inlay_hint.is_enabled(bufnr))
+      end, { desc = '(LSP) Activate/Deactivate Inlay Hints' })
+    end
   end,
 })
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,22 +1,28 @@
 local map = vim.keymap.set
 
-map('n', ';', ':')
+map('n', ';', ':', { desc = 'Command Mode' })
 
-map('n', '<C-h>', '<C-w>h')
-map('n', '<C-j>', '<C-w>j')
-map('n', '<C-k>', '<C-w>k')
-map('n', '<C-l>', '<C-w>l')
+map('n', '<C-h>', '<C-w>h', { desc = 'Move Focus to Left Window' })
+map('n', '<C-j>', '<C-w>j', { desc = 'Move Focus to Down Window' })
+map('n', '<C-k>', '<C-w>k', { desc = 'Move Focus to Top Window' })
+map('n', '<C-l>', '<C-w>l', { desc = 'Move Focus to Right Window' })
 
-map('n', 'J', 'mzJ`z')
-map('n', '<C-d>', '<C-d>zz')
-map('n', '<C-u>', '<C-u>zz')
-map('n', 'N', 'Nzzzv')
-map('n', 'n', 'nzzzv')
+map('n', 'J', 'mzJ`z', { desc = 'Merge with line below' })
+map('n', '<C-d>', '<C-d>zz', { desc = 'Scroll Down' })
+map('n', '<C-u>', '<C-u>zz', { desc = 'Scroll Up' })
+map('n', 'N', 'Nzzzv', { desc = 'Go To Prev Occurrence' })
+map('n', 'n', 'nzzzv', { desc = 'Go To Next Occurrence' })
 
-map('v', 'J', ":m '>+1<cr>gv=gv")
-map('v', 'K', ":m '<-2<cr>gv=gv")
+map('v', 'J', ":m '>+1<cr>gv=gv", { desc = 'Move 1 or more lines Down' })
+map('v', 'K', ":m '<-2<cr>gv=gv", { desc = 'Move 1 or more lines Up' })
 
-map('n', '<leader>t', vim.cmd.TroubleToggle)
-map('n', '<leader>T', vim.cmd.TroubleClose)
+map('n', '<leader>e', vim.cmd.Trouble, { desc = 'Open Diagnostics' })
 
-map('n', '<leader>z', vim.cmd.ZenMode)
+map('n', '<leader>tf', require('telescope.builtin').find_files, { desc = 'Telescope Find Files' })
+map('n', '<leader>ts', require('telescope.builtin').live_grep, { desc = 'Telescope Search' })
+map('n', '<leader>tS', require('telescope.builtin').grep_string, { desc = 'Telescope Search current Word' })
+map('n', '<leader>tb', require('telescope.builtin').buffers, { desc = 'Telescope Search Buffers' })
+
+map('n', '<leader>T', vim.cmd.TodoTrouble, { desc = 'Open Todos' })
+
+map('n', '<leader>z', vim.cmd.ZenMode, { desc = 'Toggle ZenMode' })
