6a8e262e1932b100ee52922221c1ae6c414b7917
Daniele Antimi <daniele.antimi@gmail.com>
2024-04-02 19:36:32 +0200

    chore(nvim): changed telescope's from open qflist to open trouble

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,18 +8,23 @@ return {
       build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
     },
   },
-  opts = {
-    defaults = {
-      prompt_prefix = ' ',
-      selection_caret = ' ',
-      layout_config = {
-        horizontal = {
-          prompt_position = 'top',
+  config = function()
+    local actions = require('telescope.actions')
+    local trouble_actions = require('trouble.providers.telescope')
+
+    local opts = {
+      defaults = {
+        prompt_prefix = '󰍉 ',
+        selection_caret = ' ',
+        mappings = {
+          i = {
+            ['<C-q>'] = trouble_actions.smart_open_with_trouble,
+            ['<M-q>'] = actions.nop,
+          },
         },
       },
-    },
-  },
-  config = function(_, opts)
+    }
+
     require('telescope').setup(opts)
     require('telescope').load_extension('fzf')
   end,
