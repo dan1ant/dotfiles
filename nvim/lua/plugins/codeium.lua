5898dec7c118e27fefc26eb4a469f7d144ebc970
Daniele Antimi <daniele.antimi@gmail.com>
2024-04-02 19:34:50 +0200

    chore(nvim): removed codeium plugin

--- nvim/lua/plugins/codeium.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'Exafunction/codeium.vim',
-  event = 'BufEnter',
-  init = function()
-    vim.keymap.set('i', '<C-a>', function()
-      return vim.fn['codeium#Accept']()
-    end, { expr = true, silent = true })
-  end,
-}
