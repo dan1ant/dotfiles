fae9dec00800f66fed6819d742f02d4bd6f12822
Daniele Antimi <daniele.antimi@gmail.com>
2024-04-15 18:46:48 +0200

    refactor(nvim): changed lualine config
    - new separators
    - mode icon
    - lsp client name in statusbar
    - changed progress with fileformat

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,18 +1,47 @@
 return {
   'nvim-lualine/lualine.nvim',
-  opts = {
-    options = {
-      component_separators = { left = '|', right = '|' },
-      section_separators = { left = '', right = '' },
-    },
-    sections = {
-      lualine_x = { 'filetype' },
-    },
-    extensions = {
-      'trouble',
-    },
-  },
   dependencies = {
     'nvim-tree/nvim-web-devicons',
+  },
+  opts = {
+    options = {
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+    },
+    sections = {
+      lualine_a = {
+        {
+          'mode',
+          icon = '',
+        },
+      },
+      lualine_x = {
+        {
+          -- From lualine.nvim examples
+          function()
+            local msg = 'No Active Lsp'
+            local buf_ft = vim.api.nvim_get_option_value('filetype', { buf = 0 })
+            local clients = vim.lsp.get_clients()
+            if next(clients) == nil then
+              return msg
+            end
+            for _, client in ipairs(clients) do
+              local filetypes = client.config.filetypes
+              if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                return client.name
+              end
+            end
+            return msg
+          end,
+          icon = '  LSP:',
+        },
+        'filetype',
+      },
+      lualine_y = { 'fileformat' },
+    },
+    extensions = {
+      'lazy',
+      'trouble',
+    },
   },
 }
