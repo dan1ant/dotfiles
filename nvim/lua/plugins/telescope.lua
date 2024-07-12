cce66e6f13e66fa3c0a2c68058868ca8ed378eff
Daniele Antimi <daniele.antimi@gmail.com>
2024-07-12 13:16:06 +0200

    feat(nvim): telescope.nvim config changes

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,31 +1,34 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.6',
+  tag = '0.1.8',
   dependencies = {
     'nvim-lua/plenary.nvim',
     {
       'nvim-telescope/telescope-fzf-native.nvim',
       build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
     },
+    'nvim-telescope/telescope-dap.nvim',
   },
-  config = function()
+  opts = function()
     local actions = require('telescope.actions')
-    local trouble_actions = require('trouble.providers.telescope')
+    local trouble_actions = require('trouble.sources.telescope')
 
-    local opts = {
+    return {
       defaults = {
         prompt_prefix = '🔍 ',
         selection_caret = ' ',
         mappings = {
           i = {
-            ['<C-q>'] = trouble_actions.smart_open_with_trouble,
+            ['<C-q>'] = trouble_actions.open,
             ['<M-q>'] = actions.nop,
           },
         },
       },
     }
-
+  end,
+  config = function(_, opts)
     require('telescope').setup(opts)
     require('telescope').load_extension('fzf')
+    require('telescope').load_extension('dap')
   end,
 }
