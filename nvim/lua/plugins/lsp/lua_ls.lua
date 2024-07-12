8e767d771bb8e83abd786c2cb014f88cc82aece9
Daniele Antimi <daniele.antimi@gmail.com>
2024-07-12 13:11:45 +0200

    refactor(nvim): update lua_ls config

--- nvim/lua/plugins/lsp/lua_ls.lua
+++ nvim/lua/plugins/lsp/lua_ls.lua
@@ -4,6 +4,14 @@ return {
       completion = {
         callSnippet = 'Replace',
       },
+      diagnostics = {
+        disable = {
+          'need-check-nil',
+        },
+      },
+      format = {
+        enable = false,
+      },
       hint = {
         enable = true,
       },
