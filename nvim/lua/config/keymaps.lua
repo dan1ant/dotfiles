a7dbc10ff8cff0101253e54bfd7c37d1b2da3a7b
dan1ant <21098537+dan1ant@users.noreply.github.com>
2025-04-17 10:25:45 +0200

    nvim: change lazy.nvim config structure

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,3 +10,5 @@ map('n', 'n', 'nzzzv', { desc = 'Go To Next Occurrence' })
 
 map('v', 'J', ":m '>+1<cr>gv=gv", { desc = 'Keep indentation while moving UP' })
 map('v', 'K', ":m '<-2<cr>gv=gv", { desc = 'Keep indentation while moving DOWN' })
+
+map('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit Terminal mode' })
